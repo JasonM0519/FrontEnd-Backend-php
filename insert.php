ab1f37b43cbf0dfb4cd30d25573ef0f5d08b53a6
JasonM0519 <58527129+JasonM0519@users.noreply.github.com>
2019-12-15 12:02:03 -0500

    Updated HTML
    Still not working though

---
+++ insert.php
@@ -0,0 +1,38 @@
+<?php
+
+	$con = mysqli_connect('127.0.0.1','root','');
+	
+	if(!$con)
+	{
+		echo 'Not Connected To Server';
+	}
+	
+	if(!mysqli_select_db($con'Seller_Information'));
+	{
+		echo 'Database not Selected');
+	}
+	
+	$Name = $_POST['username'];
+	$Address = $_POST['address'];
+	$City = $_POST['city'];
+	$Province = $_POST['province'];
+	$PostalCode = $_POST['postalcode'];
+	$Phone = $_POST['phone'];
+	$Email = $_POST['email'];
+	
+
+	
+	$sql = "INSERT INTO Seller_Information (name,email) VALUES ('$Name','$Address','$City','$Province','$PostalCode','$Phone','Email')";
+	
+	if(!mysqli_query($con,$sql))
+	{
+		echo 'Not Inserted';
+	}
+	else
+	{
+		echo 'Inserted';
+	}
+	
+	header("refresh:2; url=index.html.html");
+
+?>
